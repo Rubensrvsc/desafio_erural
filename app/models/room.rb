f168f70fc91b830370b489d09fab9d4729c231a7
rubensrvsc <rubensspfc100@gmail.com>
2023-02-26 17:05:50 -0300

    refactor: validates fields

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
     has_many :user_rooms
     has_many :users, through: :user_rooms
+
+    validates :number, presence: true
+    validates :link, presence: true
+    validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_many :user_rooms
     has_many :rooms, through: :user_rooms
 
+    validates :username, presence: true
+    validates :email, presence: true
+
     def user_form
         "#{username}"
     end
